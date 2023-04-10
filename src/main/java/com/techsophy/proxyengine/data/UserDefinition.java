211eb471d382881d55febce5fc534df8a08273f1
vaibhav <vaibhav.j@techsophy.com>
2023-04-10 14:01:37 +0530

    adding user definition

---
+++ src/main/java/com/techsophy/proxyengine/data/UserDefinition.java
@@ -0,0 +1,13 @@
+package com.techsophy.proxyengine.data;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+
+
+@Data
+public class UserDefinition {
+    public String preferredUsername;
+    public BigInteger userId;
+
+}
